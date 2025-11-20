d3e27e5925cd9a6d23ee0fc1fe02e7fe2427abe5
sapphicart <111532652+sapphicart@users.noreply.github.com>
2025-11-21 00:19:54 +0530

    updating eta for notice

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -473,7 +473,7 @@ The primary (and only) goal of our website is to let you access the Machine Lear
             {/* Notice Stuff */}
             <div className="flex flex-col px-10 rounded-2xl items-center justify-center bg-amber-50 border border-2 border-red-400 mt-5 text-red-500 p-4">
                     <h2 className="text-md">Server Error</h2>
-                    <p className="text-xs pt-3">BOLI server is currently down due to unforeseen circumstances.<br></br>We are working hard to fix it, please try again later.<br></br><br></br><span className="font-bold">ETA: 20th November, 11:00 PM +5:30 GMT</span></p>
+                    <p className="text-xs pt-3">BOLI server is currently down due to unforeseen circumstances.<br></br>We are working hard to fix it, please try again later.<br></br><br></br><span className="font-bold">ETA: 21th November, 12:00 PM +5:30 GMT</span></p>
             </div>
 
             
